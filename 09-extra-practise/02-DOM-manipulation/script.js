03b66c7fc8a4b8cd0aa0deaeefbeefced36553d2
EvaRoets <eva_roets@hotmail.com>
2021-07-23 13:24:35 +0200

    Create all html elements

---
+++ 09-extra-practise/02-DOM-manipulation/script.js
@@ -0,0 +1,23 @@
+const divHeader = document.createElement("div");
+const header = document.createElement("header");
+const holygrailBody = document.createElement("div");
+const holygrailNav = document.createElement("nav");
+const listNav = document.createElement("ul");
+const listItemNav = document.createElement("li");
+const link1 = document.createElement("a");
+const link2 = document.createElement("a");
+const main = document.createElement("main");
+const h1Nav = document.createElement("h1");
+const h2Nav= document.createElement("h2");
+const paragraph1 = document.createElement("p");
+const paragraph2 = document.createElement("p");
+const paragraph3 = document.createElement("p");
+const aside = document.createElement("aside");
+const imgSide = document.createElement("img");
+const divFooter = document.createElement("div");
+const footer = document.createElement("footer")
+
+
+
+
+

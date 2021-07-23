37ddc8bfc48c14cd3eab1232391601ae488c62e6
EvaRoets <eva_roets@hotmail.com>
2021-07-23 13:25:55 +0200

    Add classes to html elements

--- 09-extra-practise/02-DOM-manipulation/script.js
+++ 09-extra-practise/02-DOM-manipulation/script.js
@@ -1,21 +1,52 @@
+const body = document.querySelector("body");
+body.classList.add("holygrail-container");
+
 const divHeader = document.createElement("div");
+divHeader.classList.add("header");
+
 const header = document.createElement("header");
+
 const holygrailBody = document.createElement("div");
+holygrailBody.classList.add("holygrailBody")
+
 const holygrailNav = document.createElement("nav");
+holygrailNav.classList.add("holygrailNav");
+
 const listNav = document.createElement("ul");
 const listItemNav = document.createElement("li");
+
 const link1 = document.createElement("a");
+
 const link2 = document.createElement("a");
+
 const main = document.createElement("main");
+main.classList.add("holygrail-content");
+
 const h1Nav = document.createElement("h1");
+
 const h2Nav= document.createElement("h2");
+
 const paragraph1 = document.createElement("p");
+
 const paragraph2 = document.createElement("p");
+
 const paragraph3 = document.createElement("p");
+
 const aside = document.createElement("aside");
+aside.classList.add("holyGrailAds")
+
 const imgSide = document.createElement("img");
+
 const divFooter = document.createElement("div");
+divFooter.classList.add("footer")
+
 const footer = document.createElement("footer")
+
+
+
+
+
+
 
 
 
